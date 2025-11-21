021ba28ae511f13a583b46450e3abd143017487f
Luuxio <vayriot.bastien@gmail.com>
2025-11-21 10:43:55 +0100

    Update

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const routes = require("./routes/index");
 
 app.use(express.json());
 
@@ -8,6 +9,14 @@ app.get("/", (req, res) => {
     // res.statusCode("200");
     res.json("Page ok");
 });
+
+routes(app);
+const errorHandler = (err, req, res, next) => {
+    const { status = 500, message = 'Something went wrong!' } = err;
+
+    res.status(status).json({ error: message });
+};
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`API ready on http://localhost:${port}`);
---
+++ routes/test.js
@@ -0,0 +1,9 @@
+const jspContrl = require("../controller/test");
+
+module.exports = [
+    {
+        url: "",
+        method: "get",
+        func: []
+    }
+]
